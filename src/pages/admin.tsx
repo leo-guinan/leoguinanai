d8629ef05a1ee97bf1a7982dd4f5942d27339bba
leo-guinan <leo@copy.ai>
2023-03-12 15:11:32 -0400

    adding admin page basic flow

---
+++ src/pages/admin.tsx
@@ -0,0 +1,11 @@
+import { BlitzPage } from "@blitzjs/next"
+
+const AdminPage: BlitzPage = () => {
+  return (
+    <>
+      <h1>This is an admin page</h1>
+    </>
+  )
+}
+
+export default AdminPage
